f35a2492f1988a2ae41955ece879101114ec8afc
Pavel Semin <p.semin@corp.mail.ru>
2023-01-23 22:27:40 +0300

    sprint 2: Final tasks

---
+++ src/sprint_2/xyz_final_tasks/a/Main.java
@@ -0,0 +1,123 @@
+package sprint_2.xyz_final_tasks.a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Чтение данных
+        Scanner scanner = new Scanner(System.in);
+        int commandsCount = Integer.parseInt(scanner.nextLine());
+        int bufferSize = Integer.parseInt(scanner.nextLine());
+        String[] commands = new String[commandsCount];
+
+        for (int i = 0; i < commandsCount; i++) {
+            commands[i] = scanner.nextLine();
+        }
+
+        // Обработка данных
+        CircleBuffer buffer = new CircleBuffer(bufferSize);
+        StringBuilder sb = new StringBuilder();
+
+        for (String command : commands) {
+            handleCommand(command, buffer, sb);
+        }
+        System.out.println(sb);
+    }
+
+    private static void handleCommand(String command, CircleBuffer buffer, StringBuilder sb) {
+        if (command.equals("pop_front")) {
+            setResult(buffer.popFront(), sb);
+        } else if (command.equals("pop_back")) {
+            setResult(buffer.popBack(), sb);
+        } else if (command.startsWith("push_front")) {
+            boolean isSuccess = buffer.pushFront(getValueFromCommand(command));
+            setNegativePushResultIfNeed(isSuccess, sb);
+        } else if (command.startsWith("push_back")) {
+            boolean isSuccess = buffer.pushBack(getValueFromCommand(command));
+            setNegativePushResultIfNeed(isSuccess, sb);
+        }
+    }
+
+    private static String getValueFromCommand(String command) {
+        int startIndex = command.indexOf(" ");
+        int endIndex = command.length();
+        return command.substring(startIndex + 1, endIndex);
+    }
+
+    private static void setResult(String res, StringBuilder sb) {
+        if (res == null) {
+            sb.append("error");
+        } else {
+            sb.append(res);
+        }
+        sb.append("\n");
+    }
+
+    private static void setNegativePushResultIfNeed(boolean isSuccess, StringBuilder sb) {
+        if (!isSuccess) {
+            sb.append("error").append("\n");
+        }
+    }
+}
+
+class CircleBuffer {
+    private String[] deque;
+    private int head = 0;
+    private int tail = 0;
+    private int itemsCount = 0;
+
+    CircleBuffer(int size) {
+        deque = new String[size];
+    }
+
+    boolean pushFront(String value) {
+        if (itemsCount == deque.length) {
+            return false;
+        }
+
+        // Добавляем элемент
+        head = (head - 1 + deque.length) % deque.length;
+        deque[head] = value;
+        itemsCount++;
+        return true;
+    }
+
+    boolean pushBack(String value) {
+        if (itemsCount == deque.length) {
+            return false;
+        }
+
+        // Добавляем элемент
+        deque[tail] = value;
+        tail = (tail + 1) % deque.length;
+        itemsCount++;
+        return true;
+    }
+
+    String popFront() {
+        if (itemsCount == 0) {
+            return null;
+        }
+
+        // Ищем элемент
+        String res = deque[head];
+        deque[head] = null;
+        head = (head + 1) % deque.length;
+        itemsCount--;
+        return res;
+    }
+
+    String popBack() {
+        if (itemsCount == 0) {
+            return null;
+        }
+
+        // Ищем элемент
+        tail = (tail - 1 + deque.length) % deque.length;
+        String res = deque[tail];
+        deque[tail] = null;
+        itemsCount--;
+        return res;
+    }
+}
---
+++ src/sprint_2/xyz_final_tasks/b/Main.java
@@ -0,0 +1,58 @@
+package sprint_2.xyz_final_tasks.b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        Stack<Integer> digits = new Stack<>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            Integer digit = parseToken(token);
+            if (digit != null) {
+                digits.push(digit);
+            } else {
+                performOperation(digits, token);
+            }
+        }
+
+        System.out.println(digits.pop());
+    }
+
+    private static Integer parseToken(String token) {
+        Integer result;
+        try {
+            result = Integer.parseInt(token);
+        } catch (NumberFormatException ex) {
+            result = null;
+        }
+        return result;
+    }
+
+    private static void performOperation(Stack<Integer> digits, String token) {
+        int firstValue = digits.pop();
+        int secondValue = digits.pop();
+        switch (token) {
+            case "+":
+                digits.push(secondValue + firstValue);
+                break;
+            case "-":
+                digits.push(secondValue - firstValue);
+                break;
+            case "*":
+                digits.push(secondValue * firstValue);
+                break;
+            case "/":
+                digits.push(Math.floorDiv(secondValue, firstValue));
+                break;
+            default:
+                break;
+        }
+    }
+}
