0fbe57fb544aa76ebbe5a6b2b1c441e63f403603
Pavel Semin <p.semin@corp.mail.ru>
2023-03-21 12:43:58 +0300

    sprint 4: task h

--- src/sprint_3/c/Main.java
+++ src/sprint_3/c/Main.java
@@ -1,12 +1,14 @@
 package sprint_3.c;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String first = scanner.nextLine();
-        String second = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String first = reader.readLine();
+        String second = reader.readLine();
 
         int charIndex = 0;
         int endIndex = second.length();
--- src/sprint_4/h/Main.java
+++ src/sprint_4/h/Main.java
@@ -1,28 +1,34 @@
 package sprint_4.h;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String first = scanner.nextLine();
-        String second = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String first = reader.readLine();
+        String second = reader.readLine();
         if (first.length() != second.length()) {
             System.out.println("NO");
             return;
         }
+
         Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
         String res = "YES";
         for (int i = 0; i < first.length(); i++) {
             Character fch = first.charAt(i);
             Character sch = second.charAt(i);
             Character keyChar = map.get(fch);
-            System.out.println("fch " + fch + " , sch " + sch + " keyChar  " + keyChar);
+            Character secondKeyChar = map2.get(sch);
+
             if (keyChar == null) {
                 map.put(fch, sch);
-            } else if (!map.get(fch).equals(sch)) {
+                map2.put(sch, fch);
+            } else if (!keyChar.equals(sch) || (secondKeyChar != null && !secondKeyChar.equals(fch))) {
                 res = "NO";
                 break;
             }
