65321c341e1b1daf40b7bcc33b87538ba0f45f3e
Pavel Semin <p.semin@corp.mail.ru>
2023-03-29 16:31:33 +0300

    sprint 4: task 2

--- src/sprint_4/b/Main.java
+++ src/sprint_4/b/Main.java
@@ -1,8 +1,48 @@
 package sprint_4.b;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println((-1) % 1000);
-        System.out.println((-13) % 5);
+        int basic = 1000;
+        int module = 123987123;
+        boolean needToSearchValues = true;
+
+        while (needToSearchValues) {
+            String fStr = getRandStr();
+            String sStr = getRandStr();
+            long firstHash = getHash(fStr, basic, module);
+            long secondHash = getHash(sStr, basic, module);
+
+            if (firstHash == secondHash) {
+                System.out.println(fStr + " : " + sStr);
+                needToSearchValues = false;
+            }
+        }
+    }
+
+    private static long getHash(String value, int basis, int module) {
+        long hash = 0;
+        for (int i = 0; i < value.length(); i++) {
+            int x = value.charAt(i);
+            hash = (hash * basis + x) % module;
+        }
+
+        return hash;
+    }
+
+    private static int leftLimit = 97; // letter 'a'
+    private static int rightLimit = 122; // letter 'z'
+    private static int targetStringLength = 10;
+    private static Random random = new Random();
+
+    private static String getRandStr() {
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
